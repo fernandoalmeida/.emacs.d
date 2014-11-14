30b61ef092661deb4c017fc46748fcee50956f02
Fernando Almeida <fernando@fernandoalmeida.net>
2014-11-14 01:55:16 -0200

    Sets initial frame size and position

--- packages/init-theme.el
+++ packages/init-theme.el
@@ -5,3 +5,8 @@
 (set-face-background 'magit-item-highlight "#2c3537")
 (set-face-background 'isearch "brown")
 (set-face-background 'lazy-highlight "burlywood4")
+
+(add-to-list 'default-frame-alist '(left . 0))
+(add-to-list 'default-frame-alist '(top . 0))
+(add-to-list 'default-frame-alist '(height . 45))
+(add-to-list 'default-frame-alist '(width . 190))
