4e2c987d5df3fc1acb7c584e11f5bd9aba108227
Fernando Almeida <fernando@fernandoalmeida.net>
2014-11-21 03:32:37 -0200

    Removes comments

--- configurations/bindings.el
+++ configurations/bindings.el
@@ -3,16 +3,10 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Copy - Paste
-(setq mouse-drag-copy-region nil)  ; stops selection with a mouse being immediately injected to the kill ring
-(setq x-select-enable-primary nil) ; stops killing/yanking interacting with primary X11 selection
-(setq x-select-enable-clipboard t) ; makes killing/yanking interact with clipboard X11 selection
-
-;; You need an emacs with bug #902 fixed for this to work properly. It has now been fixed in CVS HEAD.
-;; it makes "highlight/middlebutton" style (X11 primary selection based) copy-paste work as expected
-;; if you're used to other modern apps (that is to say, the mere act of highlighting doesn't
-;; overwrite the clipboard or alter the kill ring, but you can paste in merely highlighted
-;; text with the mouse if you want to)
-(setq select-active-regions t) ; active region sets primary X11 selection
+(setq mouse-drag-copy-region nil)              ; stops selection with a mouse being immediately injected to the kill ring
+(setq x-select-enable-primary nil)             ; stops killing/yanking interacting with primary X11 selection
+(setq x-select-enable-clipboard t)             ; makes killing/yanking interact with clipboard X11 selection
+(setq select-active-regions t)                 ; active region sets primary X11 selection
 (global-set-key [mouse-2] 'mouse-yank-primary) ; make mouse middle-click only paste from primary X11 selection, not clipboard and kill ring.
 
 ;; Window Navigation
