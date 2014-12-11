68f6f4a94aee0bb0d3d4a785a16202dda310a496
Fernando Almeida <fernando@fernandoalmeida.net>
2014-12-11 02:21:11 -0200

    Add Helm configs

--- packages/init-helm.el
+++ packages/init-helm.el
@@ -1,4 +1,5 @@
-;; Helm setup
+(helm-mode t)
+
 (setq helm-quick-update                      t ; do not display invisible candidates
       helm-buffers-fuzzy-matching            t
       helm-move-to-line-cycle-in-source      t
@@ -9,14 +10,46 @@
       helm-ff-transformer-show-only-basename nil  ; preventing hidden matching issue
       helm-ls-git-show-abs-or-relative       'relative
       helm-full-frame                        t
-      helm-buffer-max-length                 50)
+      helm-buffer-max-length                 50
+      helm-bookmark-show-location            t
+      helm-projectile-sources-list           '(helm-source-projectile-buffers-list
+					       helm-source-projectile-files-list))
 
-;; Custom bingings
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(autoload 'helm-descbinds      "helm-descbinds" t)
+(autoload 'helm-eshell-history "helm-eshell"    t)
+(autoload 'helm-esh-pcomplete  "helm-eshell"    t)
+
+(add-hook 'eshell-mode-hook
+          '(lambda ()
+	     (define-key eshell-mode-map (kbd "M-r") 'helm-eshell-history)))
+
 (global-unset-key (kbd "C-x c"))
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-x f") 'helm-projectile)
-(global-set-key (kbd "C-c f") 'helm-ls-git-ls)
+(global-set-key   (kbd "M-x")     'helm-M-x)
+(global-set-key   (kbd "M-y")     'helm-show-kill-ring)
+
+(global-set-key   (kbd "C-x p")   'helm-projectile-switch-project)
+(global-set-key   (kbd "C-x b")   'helm-buffers-list)
+(global-set-key   (kbd "C-x f")   'helm-projectile)
+(global-set-key   (kbd "C-x C-f") 'helm-find-files)
+(global-set-key   (kbd "C-x r l") 'helm-filtered-bookmarks)
+
+(global-set-key   (kbd "C-c h")   'helm-command-prefix)
+(global-set-key   (kbd "C-c f")   'helm-ls-git-ls)
+(global-set-key   (kbd "C-c r")   'helm-recentf)
+
+(global-set-key   (kbd "C-h a")   'helm-apropos)
+(global-set-key   (kbd "C-h i")   'helm-info-emacs)
+(global-set-key   (kbd "C-h b")   'helm-descbinds)
+
+;; More familiar helm find-file navigation (from Renan Ranelli - github.com/rranelli)
+(defun helm-find-files-sensitive-backspace ()
+  "Deletes whole directory in helm find files mode on backspace."
+  (interactive)
+  (if (char-equal ?/ (char-before))
+      (helm-find-files-up-one-level 1)
+    (backward-delete-char 1)))
+
+(define-key helm-find-files-map (kbd "<backspace>") 'helm-find-files-sensitive-backspace)
+(define-key helm-find-files-map (kbd "<tab>")       'helm-execute-persistent-action)
+(define-key helm-find-files-map (kbd "<C-tab>")     'helm-select-action)
+
