2747b84d8db0ba27843ee1bcb24b06f170254110
Fernando Almeida <fernando@fernandoalmeida.net>
2015-02-12 03:01:52 -0200

    Change projectile indexing method to 'find'

--- setup/setup-projectile.el
+++ setup/setup-projectile.el
@@ -1,5 +1,6 @@
 (projectile-global-mode)
+
 (setq projectile-enable-caching t)
-(setq projectile-indexing-method 'git)
+(setq projectile-indexing-method 'find)
 (setq projectile-completion-system 'helm)
 (setq projectile-switch-project-action 'helm-projectile)
