93f1552cc8e80aafc309a64a52d33c17b6f6855b
Fernando Almeida <fernando@fernandoalmeida.net>
2015-03-22 02:03:03 -0300

    Remove some theme customizations

--- setup/setup-theme.el
+++ setup/setup-theme.el
@@ -3,16 +3,9 @@
 ;;; Code:
 (load-theme 'zenburn t)
 
-(set-face-background 'region "#000000")
 (set-face-background 'hl-line "#2c3537")
-(set-face-background 'magit-item-highlight "#2c3537")
 (set-face-background 'isearch "brown")
 (set-face-background 'lazy-highlight "burlywood4")
-
-(add-to-list 'default-frame-alist '(left . 0))
-(add-to-list 'default-frame-alist '(top . 0))
-(add-to-list 'default-frame-alist '(height . 45))
-(add-to-list 'default-frame-alist '(width . 190))
 (add-to-list 'default-frame-alist '(font . "Monospace 9"))
 
 (provide 'setup-theme)
