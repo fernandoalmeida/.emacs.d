b751608528989c7a1c4369e1b5b02e2f340194cb
Fernando Almeida <fernando@fernandoalmeida.net>
2015-04-16 01:25:09 -0300

    Add custom bindings to Evernote mode

--- setup/setup-evernote.el
+++ setup/setup-evernote.el
@@ -5,5 +5,9 @@
 (require 'evernote-mode)
 (setq evernote-developer-token (getenv "EVERNOTE_DEVELOPER_TOKEN"))
 
+(global-set-key (kbd "C-c e b") 'evernote-browser)
+(global-set-key (kbd "C-c e n") 'evernote-create-note)
+(global-set-key (kbd "C-c e s") 'evernote-browsing-search-notes)
+
 (provide 'setup-evernote)
 ;;; setup-evernote ends here
