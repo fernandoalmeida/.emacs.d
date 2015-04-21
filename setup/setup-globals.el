51c920678e248942627bd1cdebf3ebf6bcde956e
Fernando Almeida <fernando@fernandoalmeida.net>
2015-04-21 00:25:39 -0300

    Custom bindings to company-mode

--- setup/setup-globals.el
+++ setup/setup-globals.el
@@ -62,7 +62,13 @@
 (yas-global-mode 1)
 
 ;; Company
-(add-hook 'after-init-hook 'global-company-mode)
+(global-company-mode)
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "C-h") 'company-show-doc-buffer)
+  (define-key company-active-map (kbd "C-l") 'company-show-location)
+  (define-key company-active-map (kbd "<tab>") 'company-complete))
 
 ;; Rainbow Mode
 (add-hook 'prog-mode-hook 'rainbow-mode)
