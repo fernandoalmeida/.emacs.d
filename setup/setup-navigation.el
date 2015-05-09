4ba5a25138770ee2d929a93ceb49f3b4cd867466
Fernando Almeida <fernando@fernandoalmeida.net>
2015-05-09 03:51:22 -0300

    Move `acejump` configs to setup file

--- setup/setup-navigation.el
+++ setup/setup-navigation.el
@@ -22,24 +22,5 @@
 ;; Reversible C-x 1
 (global-set-key (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)
 
-(autoload
-  'ace-jump-mode
-  "ace-jump-mode"
-  "Emacs quick move minor mode"
-  t)
-
-(setq ace-jump-mode-move-keys  (append "asdfghjklqweiop" nil))
-(define-key global-map (kbd "C-;") 'ace-jump-mode)
-(setq ace-jump-word-mode-use-query-char nil)
-
-(autoload
-  'ace-jump-mode-pop-mark
-  "ace-jump-mode"
-  "Ace jump back:-)"
-  t)
-(eval-after-load "ace-jump-mode"
-  '(ace-jump-mode-enable-mark-sync))
-(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
-
 (provide 'setup-navigation)
 ;;; setup-navigation.el ends here
