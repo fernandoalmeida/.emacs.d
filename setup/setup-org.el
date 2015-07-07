ab0cab10052d6c221e97275103a769ccbea53711
Fernando Almeida <fernando@fernandoalmeida.net>
2015-07-07 03:56:39 -0300

    Improving Org mode configs

--- setup/setup-org.el
+++ setup/setup-org.el
@@ -1,23 +1,58 @@
 ;;; setup-org --- Setup org mode
 ;;; Commentary:
 ;;; Code:
-(setq org-directory "~/.emacs.d/org")
-(setq org-default-notes-file (concat org-directory "/notes.org"))
-(define-key global-map (kbd "C-c c") 'org-capture)
+(require 'org)
+
+(setq org-directory "~/Dropbox/fernando/org/")
+(setq org-files-regexp "\\.\\(org\\|org_archive\\|txt\\)$")
+(setq org-user-agenda-files (directory-files org-directory t org-files-regexp))
+(setq org-default-notes-file (concat org-directory "refile.org"))
+
+(add-to-list 'auto-mode-alist (cons org-files-regexp 'org-mode))
+
+(global-unset-key (kbd "C-o"))
+(global-set-key (kbd "C-o c") 'org-capture)
+(global-set-key (kbd "C-o a") 'org-agenda)
+(global-set-key (kbd "C-o l") 'org-store-link)
+(global-set-key (kbd "C-o b") 'org-iswitchb)
+
 
 (setq org-todo-keywords
-      '((sequence "TODO"
-                  "DOING"
-		  "REVIEW"
-                  "|"
-                  "DONE"
-                  "DELEGATED")))
+      (quote ((sequence "TODO(t)"
+                        "NEXT(n)"
+                        "|"
+                        "DONE(d)")
+              (sequence "WAITING(w@/!)"
+                        "HOLD(h@/!)"
+                        "|"
+                        "CANCELLED(c@/!)"))))
+
+(setq org-todo-keyword-faces
+      '(("TODO" :foreground "red" :weight bold)
+        ("NEXT" :foreground "blue" :weight bold)
+        ("DONE" :foreground "forest green" :weight bold)
+        ("WAITING" :foreground "orange" :weight bold)
+        ("HOLD" :foreground "magenta" :weight bold)
+        ("CANCELLED" :foreground "forest green" :weight bold)))
+
+(setq org-use-fast-todo-selection t)
+
+(setq org-todo-state-tags-triggers
+      (quote (("CANCELLED" ("CANCELLED" . t))
+              ("WAITING" ("WAITING" . t))
+              ("HOLD" ("WAITING") ("HOLD" . t))
+              (done ("WAITING") ("HOLD"))
+              ("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
+              ("NEXT" ("WAITING") ("CANCELLED") ("HOLD"))
+              ("DONE" ("WAITING") ("CANCELLED") ("HOLD")))))
 
 (setq org-capture-templates
-      '(("t" "Task" entry (file+headline "~/.emacs.d/org/tasks.org" "Tasks")
-	 "* TODO %?\n %i\n %a")
-	("n" "Note" entry (file+datetree "~/.emacs.d/org/notes.org" "Notes")
-	 "* %?\nEntered on %U\n %i\n %a")))
+      '(("t" "todo" entry (file org-default-notes-file)
+	 "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+	("n" "note" entry (file org-default-notes-file)
+	 "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+	("h" "Habit" entry (file org-default-notes-file)
+	 "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n")))
 
 (provide 'setup-org)
 ;;; setup-org ends here
