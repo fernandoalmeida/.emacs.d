1912981e564763365811578ca54a49702a8602c4
Fernando Almeida <fernando@fernandoalmeida.net>
2017-06-09 04:14:01 -0300

    Setup `JSX` mode

---
+++ setup/setup-javascript.el
@@ -0,0 +1,8 @@
+;;; setup-javascript --- Setup javascript mode
+;;; Commentary:
+;;; Code:
+
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
+
+(provide 'setup-javascript)
+;;; setup-javascript ends here
