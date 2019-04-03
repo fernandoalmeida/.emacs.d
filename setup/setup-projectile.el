d139349483151f1a4c01c1cc59a639195d88533b
Fernando Almeida <fernando@fernandoalmeida.net>
2019-04-03 16:01:46 -0300

    [projectile] Fix configs (indexing and keybinding prefix)

--- setup/setup-projectile.el
+++ setup/setup-projectile.el
@@ -4,7 +4,7 @@
 (projectile-global-mode)
 
 (setq projectile-enable-caching t)
-(setq projectile-indexing-method 'find)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (setq projectile-completion-system 'helm)
 (setq projectile-switch-project-action 'helm-projectile)
 
