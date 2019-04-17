277a90d10ef400ada4d0fa69d43a6f67d3d93bff
Fernando Almeida <fernando@fernandoalmeida.net>
2019-04-17 11:04:54 -0300

    [javascript] Customize default javascript indentation

--- setup/setup-javascript.el
+++ setup/setup-javascript.el
@@ -2,7 +2,11 @@
 ;;; Commentary:
 ;;; Code:
 
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
+(setq-default indent-tabs-mode nil)
+(setq-default js2-basic-offset 2)
+(setq js-indent-level 2)
 
 (provide 'setup-javascript)
 ;;; setup-javascript ends here
