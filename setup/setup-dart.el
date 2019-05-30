c507a520eb4ae351e20445fd0e960939f2033569
Fernando Almeida <fernando@fernandoalmeida.net>
2019-05-30 01:03:36 -0300

    [dart] Add custom function to wrap with widget

--- setup/setup-dart.el
+++ setup/setup-dart.el
@@ -11,11 +11,23 @@
 (define-key dart-mode-map (kbd "C-M-.") 'dart-find-refs)
 (define-key dart-mode-map (kbd "C-c i") 'dart-format)
 
-(defun custom/run-dart-code ()
+(defun custom/dart-run-code ()
   "Run a dart code."
   (interactive)
   (shell-command (concat dart-sdk-path "/bin/dart " (buffer-file-name))))
-(define-key dart-mode-map (kbd "C-c C-c") 'custom/run-dart-code)
+(define-key dart-mode-map (kbd "C-c C-c") 'custom/dart-run-code)
+
+(defun custom/dart-wrap-with ()
+  "Wrap with a widget"
+  (interactive)
+  (let* ((widget (read-string "Wrap with: "))
+         (start (region-beginning))
+         (end (region-end))
+        (selection (delete-and-extract-region start end))
+        (wrapped (concat widget "(\n child: " selection "\n),")))
+    (insert wrapped)
+    (dart-format)))
+(define-key dart-mode-map (kbd "C-c .") 'custom/dart-wrap-with)
 
 (provide 'setup-dart)
 ;;; setup-dart ends here
