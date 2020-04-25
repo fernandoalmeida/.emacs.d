518962ba0fcf05263ca91246ca8b0ea685ed525a
Fernando Almeida <fernando@fernandoalmeida.net>
2020-04-25 19:44:13 -0300

    [general] Allow `setenv` and `eval` on `.dir-locals`

--- setup/setup-safe-local-variables.el
+++ setup/setup-safe-local-variables.el
@@ -3,6 +3,8 @@
 ;;; Code:
 
 (put 'alchemist-mix-command 'safe-local-variable #'stringp)
+(put 'setenv 'safe-local-variable (lambda (_) t))
+(put 'eval 'safe-local-variable (lambda (_) t))
 
 ;; (add-to-list 'safe-local-variable-values
 ;;              '(alchemist-mix-command . "docker-compose run atena /usr/local/bin/mix"))
