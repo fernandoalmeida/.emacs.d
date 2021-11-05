f94097bc176949edf34b20921fe34a1f6b621892
Fernando Almeida <fernando@fernandoalmeida.net>
2021-11-05 09:06:49 -0300

    [javascript] Change default javascript mode

--- setup/setup-javascript.el
+++ setup/setup-javascript.el
@@ -2,7 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-jsx-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx$" . typescript-mode))
 (setq-default indent-tabs-mode nil)
