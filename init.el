bf5a87abb4048c9ba6794092b1045edb8005f0a3
Fernando Almeida <fernando@fernandoalmeida.net>
2023-07-03 22:15:11 -0300

    [straight] Setup `straight.el`

--- init.el
+++ init.el
@@ -1,6 +1,19 @@
 ;;; init --- Setup Emacs initialization process
 ;;; Commentary:
 ;;; Code:
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 (let ((default-directory "~/.emacs.d/vendor/"))
   (normal-top-level-add-to-load-path '("."))
   (normal-top-level-add-subdirs-to-load-path))
